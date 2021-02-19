a211d7d35b0add31269de5175645c6a2b8f2936f
Akmal Luthfi <79119115+akmalluthfi@users.noreply.github.com>
2021-02-19 19:40:28 +0700

    menambahkan komentar
    menambahkan komentar untuk memperjelas kode program

--- ganjiil-genap.cpp
+++ ganjiil-genap.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main(){
 	
-	int bil;
-	cout<<"Program menentukan bilangan bulat" <<endl;
+	int bil; // inisialisasi int bil 
+	cout<<"Program menentukan bilangan bulat" <<endl; //membuat judul program 
 	cout<<"Masukkan bilangan bulat = "; cin>> bil;
 
-	if(bil == 1 - 10000000){
+	if(bil == 1 - 10000000){ // memastikan input adalah bilangan bulat dari range 1-10.000.000
 		
-		if(bil % 2 == 0){
+		// Bilangan Ganjil adalah bilangan yang tidak bisa dibagi 2
+		// Bilangan Genap adalah bilangan yang bisa dibagi 2  
+		if(bil % 2 == 0){ // menentukan input termasuk bilangan genap atau ganjil 
 			cout<<endl<<"Bilangan Genap";
 		} else {
 			cout<<endl<<"Bilangan Ganjil";
